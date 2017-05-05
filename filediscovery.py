c4e55392a52d88a0e84e99ec985c4c9a206f6b15
gabormarosi <gabor.marosi@hotmail.com>
2017-05-05 12:50:53 +0200

    file discovery 2

--- filediscovery.py
+++ filediscovery.py
@@ -1,2 +1,18 @@
 import os
-print(os.listdir("/home/mgabee/playground"))
+def search(path):
+    files = []
+    filelist = os.listdir(path)
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            #print("add dir: "+path+"/"+fname)
+            files.extend(search(path+"/"+fname))
+            #print("dir found: "+path+"/"+fname)
+        elif fname[-3:] == ".py":
+            #print("py found: "+fname)
+            files.append(path+"/"+fname)
+            #print("files: "+files)
+    #print(filelist)
+    return files
+#files = search(os.path.abspath("/"))
+files = search(os.path.abspath("/home/mgabee/playground"))
+print(files)
