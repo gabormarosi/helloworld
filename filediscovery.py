9c4e5017ef9dc4fd354a761dbb7d945ec149fbfb
gabormarosi <gabor.marosi@hotmail.com>
2017-05-05 14:35:29 +0200

    file discovery / extension moved to param

--- filediscovery.py
+++ filediscovery.py
@@ -1,18 +1,18 @@
 import os
-def search(path):
+def search(path,ext):
     files = []
     filelist = os.listdir(path)
     for fname in filelist:
         if os.path.isdir(path+"/"+fname):
             #print("add dir: "+path+"/"+fname)
-            files.extend(search(path+"/"+fname))
+            files.extend(search(path+"/"+fname,".py"))
             #print("dir found: "+path+"/"+fname)
-        elif fname[-3:] == ".py":
+        elif fname[-3:] == ext:
             #print("py found: "+fname)
             files.append(path+"/"+fname)
             #print("files: "+files)
     #print(filelist)
     return files
-#files = search(os.path.abspath("/"))
-files = search(os.path.abspath(os.path.expanduser("~")+"/playground"))
+#files = search(os.path.abspath("/"),".py")
+files = search(os.path.abspath(os.path.expanduser("~")+"/playground"),".py")
 print(files)
